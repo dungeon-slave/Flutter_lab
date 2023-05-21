e2027739ed860c6ef0a079588b11b08f2a059ed0
snus <vasilevsky.matvey@gmail.com>
2023-05-21 14:55:28 +0300

    end task1

---
+++ 2. Introduction to Dart/tasks/task1.dart
@@ -0,0 +1,49 @@
+void main(List<String> args) {
+    print(getCoinsCount([2, 1], 27));
+}
+
+int getCoinsCount(List<int> coins, int amount)
+{
+    if (!checkConstrains(coins, amount)) {
+        return -1;
+    }
+
+    int coinsCount = 0;
+    coins.sort();
+
+    do {
+        int currAmount = amount;
+
+        for (var i = coins.length - 1; i >= 0; i--) {
+            currAmount = amount - coins[i];
+            if (currAmount >= 0) {
+                coinsCount++;
+                break;
+            }
+        }
+        if (currAmount < 0) {
+            return -1;
+        }
+
+        amount = currAmount;
+    } while (amount > 0);
+
+    return coinsCount;
+}
+
+bool checkConstrains(List<int> coins, int amount)
+{
+    if ((amount >= 0 && amount <= 104) && (coins.length >= 1 && coins.length <= 12)) 
+    {
+        for (var i = 0; i < coins.length; i++) {
+            if (!(coins[i] >= 1 && coins[i] <= 230)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
